54778956d17d1239c619a1efe9bae8b3c6fff0ac
Gea-Suan Lin <gslin@gslin.org>
2025-05-10 20:10:00 +0800

    chore: add a test job.

--- daemon/app.py
+++ daemon/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from dotenv import load_dotenv
+import os
+import requests
 
-BlockingScheduler().start()
+def SlackTestJob():
+    url = os.environ.get('SLACK_WEBHOOK_URL')
+
+def main():
+    load_dotenv()
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(SlackTestJob, 'interval', minutes=1)
+    scheduler.start()
+
+if __name__ == '__main__':
+    main()
